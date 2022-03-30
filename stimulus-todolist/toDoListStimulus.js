5239a8c9a557ad8209bcdd99c9f40d052e104989
umer-iqbal-brainx <umer.iqbal@brainxtech.com>
2022-03-30 19:54:35 +0500

    stimulusJs task v1

---
+++ stimulus-todolist/toDoListStimulus.js
@@ -0,0 +1,78 @@
+import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+        window.Stimulus = Application.start()    
+        Stimulus.register("toDoList", class extends Controller {
+          static targets = ["jobs", "allTasksDiv"]    
+          addJob(){
+            var classthis = this;
+            let individualJobDiv = document.createElement("div");
+            individualJobDiv.className = "task-div";                
+            let btnDelete = this.createrButton("button", "submit", "Delete", "delete-btn"); 
+            let btnEdit = this.createrButton("button", "submit", "Edit", "edit-btn");
+            btnDelete.addEventListener("click", function(){
+              this.parentNode.remove();
+            });              
+            btnEdit.addEventListener("click",function(){
+              this.style.display = "none";              
+              let editField = document.createElement("input");
+              editField.type = "text";
+              editField.value = this.parentNode.firstChild.nextSibling.innerHTML;
+              this.parentNode.firstChild.nextSibling.innerHTML = "";
+              this.parentNode.firstChild.nextSibling.appendChild(editField);              
+              var btnEditThis = this; 
+              let btnSave = classthis.createrButton("button", "submit", "Save", "save-btn");
+              btnSave.addEventListener("click", function(){    
+                console.log("jjj: ",this.parentElement.firstChild.nextElementSibling.firstChild.value );
+                let value1 = this.parentElement.firstChild.nextElementSibling.firstChild.value ;
+                console.log("value1: ",this.previousSibling)
+                this.parentElement.firstChild.nextElementSibling.innerHTML = "";
+                this.parentElement.firstChild.nextElementSibling.innerHTML = value1;
+                console.log("save parent: ", this.parentNode);
+                this.remove();
+                btnEditThis.style.display = "block";                
+              });
+              individualJobDiv.appendChild(btnSave);
+            })            
+            let eachJobCheckBox = this.checkBox()            
+            eachJobCheckBox.addEventListener("click", function(){
+              if (this.checked == true){
+                  console.log("i am checked");
+                  console.log(this.nextSibling);
+                  this.nextSibling.style.textDecoration = "line-through";                
+              }else{
+                  console.log("i am not checked");
+                  this.nextSibling.style.textDecoration = "none";
+              }
+            });    
+            if(event.keyCode == 13 && this.jobsTarget.value !=""){
+              individualJobDiv.appendChild(eachJobCheckBox);
+              individualJobDiv.appendChild(this.spanElement(this.jobsTarget.value ));
+              individualJobDiv.appendChild(btnDelete);
+              individualJobDiv.appendChild(btnEdit); 
+              this.allTasksDivTarget.appendChild(individualJobDiv); 
+              this.jobsTarget.value = "";
+            }       
+          }
+          // FUNCATIONS
+          createrButton(elementName, elementType, innerText, buttonClassName){
+            var button = document.createElement(elementName);
+            button.innerHTML = innerText;
+            button.value = innerText;
+            button.type = elementType;
+            button.className = buttonClassName;
+            return button;    
+        }
+        spanElement(innerHTMLValue){
+            let spanElement = document.createElement("span");
+            spanElement.id = "lable";
+            spanElement.innerHTML = innerHTMLValue;
+            return spanElement;
+        }
+        checkBox(){
+            let checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            checkBox.className = "check-box";
+            return checkBox;
+        }          
+      });
+
+
